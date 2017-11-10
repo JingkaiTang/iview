d068de85e0bc6a96d95b715212796f4788e9644b
Jingkai Tang <jingkaitang@qq.com>
2017-11-10 10:01:07 +0800

    Modal button icon

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -16,8 +16,8 @@
                         <div :class="[prefixCls + '-body']"><slot></slot></div>
                         <div :class="[prefixCls + '-footer']" v-if="!footerHide">
                             <slot name="footer">
-                                <i-button type="text" size="large" @click.native="cancel">{{ localeCancelText }}</i-button>
-                                <i-button type="primary" size="large" :loading="buttonLoading" @click.native="ok">{{ localeOkText }}</i-button>
+                                <i-button type="text" size="large" icon="close-round" @click.native="cancel">{{ localeCancelText }}</i-button>
+                                <i-button type="primary" size="large" icon="checkmark-round" :loading="buttonLoading" @click.native="ok">{{ localeOkText }}</i-button>
                             </slot>
                         </div>
                     </div>
